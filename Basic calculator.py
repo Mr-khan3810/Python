524795aad8592e2e92d962fd3afd638fa62dc7a0
Abdullah Khan <shahzadkhan12030000@gmail.com>
2024-09-08 11:19:12 +0500

    Add files via upload

---
+++ Basic calculator.py
@@ -0,0 +1,8 @@
+a=input("Enter 1st number:")
+b=input("Enter 2nd number:")
+print("The addition of ",a," and ",b," is ",int(a)+int(b))
+print("The subtraction of ",a, "and ",b," is ",int(a)-int(b))
+print("The multiplication of ",a, "and ",b," is ",int(a)*int(b))
+print("The division of ",a, "and ",b," is ",int(a)/int(b)) #With fraction
+print("The division of ",a, "and ",b," is ",int(a)//int(b)) #Without fraction
+print("The modulus of ",a, "and ",b," is ",int(a)%int(b))
---
+++ String maniuplation.py
@@ -0,0 +1,8 @@
+name="Ali"
+print(name[0])
+print(name[1])
+print(name[2])
+# Another method
+print(name[0:2])
+father="Moin,Shah"
+print(father[0:3]) #It will show the value of string of varaiable father from index 0 to 2
