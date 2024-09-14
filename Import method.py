60564c38b21ccc0f51644ea6beac56ff37dc1024
Abdullah Khan <shahzadkhan12030000@gmail.com>
2024-09-14 18:34:43 +0500

    Add files via upload

---
+++ Import method.py
@@ -0,0 +1,6 @@
+# import math
+from math import sqrt
+print(sqrt(25))
+from math import pi as p
+print(2*p)
+# print(dir(math))
---
+++ Short hand if-else.py
@@ -0,0 +1,2 @@
+a=input("Enter a number:")
+print("A+") if(int(a)>50) else print("F")
